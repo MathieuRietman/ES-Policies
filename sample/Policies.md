ede6c803a12fec99d49aa0c5493af5c7084ea56a
Mathieu Rietman <marietma@microsoft.com>
2021-01-25 11:37:44 +0100

    checkin before existance

--- sample/Policies.md
+++ sample/Policies.md
@@ -1,12 +1,14 @@
 
 
 
-Added Custom policies 
+Added Custom policies feb 2020
 
-|Type|Template file|Deploy|
+|Type|Description|Template file|Deploy|
 |------------|-------------------|----------------|
-|custom|[AppService_RequireLatestTls_Append.json](../esPoliciesDefinitionsCustom/AppService_RequireLatestTls_Append.json) | [![Deploy to Azure](https://docs.microsoft.com/en-us/azure/governance/policy/media/deploy/deploybutton.png)](https://portal.azure.com/?#blade/Microsoft_Azure_Policy/CreatePolicyDefinitionBlade/uri/https%3A%2F%2Fraw.githubusercontent.com%2FMathieuRietman%2Fes-Policies%2Fmain%2FesPoliciesDefinitionsCustom%2FAppService_RequireLatestTls_Append.json)|
+|custom||[AppService_RequireLatestTls_Append.json](../esPoliciesDefinitionsCustom/AppService_RequireLatestTls_Append.json) | [![Deploy to Azure](https://docs.microsoft.com/en-us/azure/governance/policy/media/deploy/deploybutton.png)](https://portal.azure.com/?#blade/Microsoft_Azure_Policy/CreatePolicyDefinitionBlade/uri/https%3A%2F%2Fraw.githubusercontent.com%2FMathieuRietman%2Fes-Policies%2Fmain%2FesPoliciesDefinitionsCustom%2FAppService_RequireLatestTls_Append.json)|
 |custom|[AppService_HttpsOnly_Append.json](../esPoliciesDefinitionsCustom/AppService_HttpsOnly_Append.json) | [![Deploy to Azure](https://docs.microsoft.com/en-us/azure/governance/policy/media/deploy/deploybutton.png)](https://portal.azure.com/?#blade/Microsoft_Azure_Policy/CreatePolicyDefinitionBlade/uri/https%3A%2F%2Fraw.githubusercontent.com%2FMathieuRietman%2Fes-Policies%2Fmain%2FesPoliciesDefinitionsCustom%2FAppService_HttpsOnly_Append.json)|
 |custom|[MySQL_EnableSSL_Append.json](../esPoliciesDefinitionsCustom/MySQL_EnableSSL_Append.json]) | [![Deploy to Azure](https://docs.microsoft.com/en-us/azure/governance/policy/media/deploy/deploybutton.png)](https://portal.azure.com/?#blade/Microsoft_Azure_Policy/CreatePolicyDefinitionBlade/uri/https%3A%2F%2Fraw.githubusercontent.com%2FMathieuRietman%2Fes-Policies%2Fmain%2FesPoliciesDefinitionsCustom%2FMySQL_EnableSSL_Append.json)|
-|custom|[MySQL_minTlsVerson.json](../esPoliciesDefinitionsCustom/MySQL_minTlsVerson.json) | [![Deploy to Azure](https://docs.microsoft.com/en-us/azure/governance/policy/media/deploy/deploybutton.png)](https://portal.azure.com/?#blade/Microsoft_Azure_Policy/CreatePolicyDefinitionBlade/uri/https%3A%2F%2Fraw.githubusercontent.com%2FMathieuRietman%2Fes-Policies%2Fmain%2FesPoliciesDefinitionsCustom%2FMySQL_minTlsVerson.json)|
+|custom|Enforce MySQL TLS version and  via Deploy if not exist.|[MySQL_TlsVerson_Deploy.json](../esPoliciesDefinitionsCustom/MySQL_minTlsVerson.json) | [![Deploy to Azure](https://docs.microsoft.com/en-us/azure/governance/policy/media/deploy/deploybutton.png)](https://portal.azure.com/?#blade/Microsoft_Azure_Policy/CreatePolicyDefinitionBlade/uri/https%3A%2F%2Fraw.githubusercontent.com%2FMathieuRietman%2Fes-Policies%2Fmain%2FesPoliciesDefinitionsCustom%2FMySQL_TlsVerson_Deploy.json)|
+|custom|Enforce PostgreSQL TLS version and  via Deploy if not exist.|[PostgreSQL_TlsVerson_Deploy.json](../esPoliciesDefinitionsCustom/PostgreSQL_minTlsVerson.json) | [![Deploy to Azure](https://docs.microsoft.com/en-us/azure/governance/policy/media/deploy/deploybutton.png)](https://portal.azure.com/?#blade/Microsoft_Azure_Policy/CreatePolicyDefinitionBlade/uri/https%3A%2F%2Fraw.githubusercontent.com%2FMathieuRietman%2Fes-Policies%2Fmain%2FesPoliciesDefinitionsCustom%2FPostgreSQL_TlsVerson_Deploy.json)|
+
 
---
+++ scripts/DeployCustomPolicyToMNG.ps1
@@ -0,0 +1,141 @@
+<#
+	.SYNOPSIS
+        1. Deploys one policy to mangement group
+
+
+
+	.DESCRIPTION
+
+         
+
+
+	.EXAMPLE
+	   .\DeployPolicyToMNG.ps1 -topLevelManagementGroupPrefix MRIT -file <File name From esPoliciesDefinitionsCustom> 
+	   
+	.LINK
+
+	.Notes
+		NAME:      DeployPolicyToMNG
+		AUTHOR(s): Mathieu Rietman <marietma@microsoft.com>
+		LASTEDIT:  12-10-2020
+		KEYWORDS:  policy management Management
+#>
+
+[cmdletbinding()] 
+Param (
+
+
+
+    [Parameter(Mandatory = $true)]
+    [string]$topLevelManagementGroupPrefix ,
+
+    [string]$location = "WestEurope",
+
+    
+    [Parameter(Mandatory = $true)]
+    [argumentcompleter( { (get-childitem "$PSScriptRoot/../esPoliciesDefinitionsCustom/*.json").name })]
+    [string]$file,
+
+    [string]$environment = "AzureCLoud"
+
+
+)
+function Add-AzureRequestManagemenytAPI {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        $Uri,
+        [Parameter(Mandatory = $true, Position = 1)]
+        $token,
+        [Parameter(Mandatory = $true, Position = 2)]
+        $Body,
+        [Parameter( Position = 3)]
+        $Method = "Get"
+            
+    )
+    process {
+        ## Write-Host "Execute " $Uri
+        ## Write-Host $token.accesToken
+        $Params = @{ 
+            ContentType = 'application/json'
+            Headers     = @{ 
+                'authorization' = "Bearer $($token.AccessToken)"
+            }
+            Method      = $Method
+            Uri         = $Uri
+            Body        = $Body | ConvertTo-Json -Depth 100 
+        }
+        Try { 
+            $Response = Invoke-WebRequest @Params
+        } 
+        catch {
+
+            Write-Host "Error Execute " $Uri
+            
+            Write-Host "Error Response " $_.Exception  ": " $_.ErrorDetails
+            $response = @()
+            $response  | Add-Member -MemberType NoteProperty -Name "StatusCode"  -Value  $_.Exception 
+            $response | Add-Member -MemberType NoteProperty -Name "StatusDescription"  -Value  $_.ErrorDetails
+
+            Return $Response
+        }
+    
+
+
+        If (($response.StatusCode -eq 200) -and ($Method -eq "Get")  ) {
+
+            return $Response.Content | ConvertFrom-Json
+    
+        }
+        elseif ((($response.StatusCode -eq 201) -or ($response.StatusCode -eq 200)) -and ($Method -eq "Put")  ) {
+
+            return $Response
+
+        }
+        else {
+            Write-Host "Error Execute " $Uri
+            
+            Write-Host "Error Response " $response.StatusCode  ": " $response.StatusDescription
+            return $Response
+        }
+    }
+}
+
+$context = Get-AzContext 
+
+# Get a token 
+$azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+$profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($azProfile)
+$token = $profileClient.AcquireAccessToken($context.Tenant.Id)
+  
+
+$Azureurl = Get-AzEnvironment -Name $environment
+
+
+
+
+$policyfile = "$PSScriptRoot/../esPoliciesDefinitionsCustom/$file"
+
+If (Test-Path -Path $policyfile  ) {
+
+
+
+    [PSObject]$definitionConfig = Get-Content    $policyfile | ConvertFrom-Json 
+    $definitionName = "$( $definitionConfig.name)"
+
+        
+    $Uri = "$($Azureurl.ResourceManagerUrl)providers/Microsoft.Management/managementgroups/$($topLevelManagementGroupPrefix)/providers/Microsoft.Authorization/policyDefinitions/$($definitionName)?api-version=2019-09-01"
+    $Method = "Put"
+    $Body = $definitionConfig
+    $response = Add-AzureRequestManagemenytAPI -uri $Uri -token  $token -body $body  -method $Method
+        
+    $status = $response.StatusDescription
+    $status 
+      
+        
+
+
+}
+else {
+    write-host "Error file not founct"
+}
